6aea4dea95a026077a9566a5fc80947faec41f35
Muzafar-Ali <84478313+Muzafar-Ali@users.noreply.github.com>
2024-08-04 09:03:37 -0700

    Update category.services.ts

--- src/services/category.services.ts
+++ src/services/category.services.ts
@@ -1,51 +1,51 @@
-import { CheekMakeupCategory } from "../models/categories/cheekMakeup.model";
-import { EyesMakeupCategory } from "../models/categories/eyesMakeup.model";
-import { FeaturedCategory } from "../models/categories/featuredMakeup.model";
-import { LipsMakeupCategory } from "../models/categories/lipsMakeup.model";
-import ErrorHandler from "../utils/errorClass";
+// import { CheekMakeupCategory } from "../models/categories/cheekMakeup.model";
+// import { EyesMakeupCategory } from "../models/categories/eyesMakeup.model";
+// import { FeaturedCategory } from "../models/categories/featuredMakeup.model";
+// import { LipsMakeupCategory } from "../models/categories/lipsMakeup.model";
+// import ErrorHandler from "../utils/errorClass";
 
-export const createCategory = async (name: string, categoryModel: any) => {
-  try {
-    if (!name) throw new ErrorHandler(400, "input field missing");
+// export const createCategory = async (name: string, categoryModel: any) => {
+//   try {
+//     if (!name) throw new ErrorHandler(400, "input field missing");
 
-    const isCategoryExist = await categoryModel.findOne({ name });
+//     const isCategoryExist = await categoryModel.findOne({ name });
 
-    if (isCategoryExist) throw new ErrorHandler(400, "Category already exist");
+//     if (isCategoryExist) throw new ErrorHandler(400, "Category already exist");
 
-    const category = await categoryModel.create({ name });
+//     const category = await categoryModel.create({ name });
 
-    if (!category) throw new ErrorHandler(500, "Category not created");
+//     if (!category) throw new ErrorHandler(500, "Category not created");
 
-    return category;
-  } catch (error) {
-    throw error;
-  }
-};
+//     return category;
+//   } catch (error) {
+//     throw error;
+//   }
+// };
 
-export const getCategory = async (categoryModel: any, categoryName?: string) => {
-  try {
-    let category;
+// export const getCategory = async (categoryModel: any, categoryName?: string) => {
+//   try {
+//     let category;
 
-    if (categoryName) category = await categoryModel.find({slug: categoryName}).populate("products");
-    if (!categoryName) category = await categoryModel.find();
+//     if (categoryName) category = await categoryModel.find({slug: categoryName}).populate("products");
+//     if (!categoryName) category = await categoryModel.find();
 
-    if (!category) throw new ErrorHandler(404, "Category not found");
+//     if (!category) throw new ErrorHandler(404, "Category not found");
 
-    return category;
-  } catch (error) {
-    throw error;
-  }
-};
+//     return category;
+//   } catch (error) {
+//     throw error;
+//   }
+// };
 
-export const findCategoryIdBySlug = async (slug: string) => {
-  try {
-    const category = await FeaturedCategory.findOne({ slug });
-    if (category) {
-      return category._id;
-    } else {
-      throw new Error("Category not found");
-    }
-  } catch (error) {
-    throw error;
-  }
-};
+// export const findCategoryIdBySlug = async (slug: string) => {
+//   try {
+//     const category = await FeaturedCategory.findOne({ slug });
+//     if (category) {
+//       return category._id;
+//     } else {
+//       throw new Error("Category not found");
+//     }
+//   } catch (error) {
+//     throw error;
+//   }
+// };
