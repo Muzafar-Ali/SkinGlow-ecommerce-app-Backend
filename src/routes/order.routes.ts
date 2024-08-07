fa32c4e809756e81a48e7a168127c3a64f9094b8
Muzafar-Ali <84478313+Muzafar-Ali@users.noreply.github.com>
2024-08-06 18:41:15 -0700

    Update order.routes.ts

--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -1,10 +1,10 @@
 import express from "express";
-import { createOrderWithoutUserHandler } from "../controllers/order.controllers";
+import { createOrderWithoutUserHandler, stripeWebhookHandler } from "../controllers/order.controllers";
 
 const router = express.Router();
 
 router.post("/orders", createOrderWithoutUserHandler)
-router.post('/stripewebhook',express.raw({type: 'application/json'}), )
+router.post('/stripewebhook', express.raw({type: 'application/json'}), stripeWebhookHandler)
 
 export default router;
 
